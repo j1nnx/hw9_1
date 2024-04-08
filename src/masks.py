ea524e2d2f755b1e2926395c475955c1e6c7e09e
Arkadiy <neykerez@gmail.com>
2024-04-08 08:58:11 +0300

    file fixes

--- src/masks.py
+++ src/masks.py
@@ -1,27 +1,16 @@
-from datetime import datetime
+def mask_credit_card_number(card_number: str) -> str:
+    masked_number = f'{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}'
+    return masked_number
 
 
-def mask_card_or_account(input_str):
-    """принимает на вход строку информацией тип карты/счета и номер карты/счета"""
+def mask_account_number(account_number: str) -> str:
+    masked_number = f'**{account_number[-4:]}'
+    return masked_number
 
-    def mask_card(card_num):
-        """возвращает эту строку с замаскированным номером карты"""
-        if len(card_num) < 16:
-            return card_num
-        return card_num[:8] + " ** **" + card_num[-4:]
+credit_card_number = input()
+masked_credit_card = mask_credit_card_number(credit_card_number)
+print(masked_credit_card)
 
-    def mask_account(account_num):
-        """возвращает эту строку с замаскированным номером счета."""
-        return "**" + account_num[-4:]
-
-    words = input_str.split()
-    if words[0].lower() in ["visa", "mastercard", "maestro"]:
-        return words[0] + " " + " ".join(words[1:-1]) + " " + mask_card(words[-1])
-    elif words[0].lower() == "счет":
-        return "Счет " + mask_account(words[1])
-
-
-def convert_data(input_data):
-    """функцию, которая принимает на вход строку, вида "2018-07-11T02:26:18.671407" и возвращает 11.07.2018"""
-    date_obj = datetime.strptime(input_data, "%Y-%m-%d %H:%M:%S.%f")
-    return date_obj.strftime("%d.%m.%Y")
+account_number = input()
+masked_account = mask_account_number(account_number)
+print(masked_account)
