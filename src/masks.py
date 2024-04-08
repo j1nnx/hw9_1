705495c81897548dd949412d95bc1a46e1dbb013
Arkadiy <neykerez@gmail.com>
2024-04-08 10:11:17 +0300

    add src

--- src/masks.py
+++ src/masks.py
@@ -6,12 +6,3 @@ def mask_credit_card_number(card_number: str) -> str:
 def mask_account_number(account_number: str) -> str:
     masked_number = f"**{account_number[-4:]}"
     return masked_number
-
-
-credit_card_number = input()
-masked_credit_card = mask_credit_card_number(credit_card_number)
-print(masked_credit_card)
-
-account_number = input()
-masked_account = mask_account_number(account_number)
-print(masked_account)
--- src/widget.py
+++ src/widget.py
@@ -1,21 +1,19 @@
-def mask_number(number_info: str) -> str:
-    """Функция возвращает исходную строку с замаскированным номером карты/счета."""
-    parts = number_info.split()
-    if len(parts) < 2:
-        return number_info
-    masked_part = ""
-    if parts[0] == "Счет":
-        number_type = "Счет"
-        account_number = parts[1]
-        masked_part = "**" + account_number[-4:]
+from src.masks import mask_account_number, mask_credit_card_number
+
+
+def masks_of_cards(data: str) -> str:
+    """ "
+    Функция переиспользует ранее написанные функции
+    и возвращает исходную строку с замаскированным номером карты/счета.
+    """
+    right_data = data.split(" ")
+    if right_data[0] == "Счет":
+        return f"Счет {mask_account_number(right_data[-1])}"
     else:
-        number_type = parts[0]
-        card_number = parts[1]
-        masked_part = card_number[:4] + " " + "*" * 2 + "*" * 4 + " " + "****" + " " + card_number[-4:]
-    return f"{number_type} {masked_part}"
+        return f'{" ".join(right_data[:-1])} {mask_credit_card_number(right_data[-1])}'
+
 
 def convert_datetime_to_date(datetime_string: str) -> str:
     """Функция, которая принимает строку и возвращает строку с датой"""
-    date_parts = datetime_string.split('T')[0].split('-')
-    return f'{date_parts[2]}.{date_parts[1]}.{date_parts[0]}'
-
+    date_parts = datetime_string.split("T")[0].split("-")
+    return f"{date_parts[2]}.{date_parts[1]}.{date_parts[0]}"
