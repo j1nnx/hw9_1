0f421681f04fd44dc302453f56ccf4c025bb6262
Arkadiy <neykerez@gmail.com>
2024-04-12 13:48:25 +0300

    add function filter_dicts_by_state

--- src/widget.py
+++ src/widget.py
@@ -2,15 +2,15 @@ from src.masks import mask_account_number, mask_credit_card_number
 
 
 def masks_of_cards(data: str) -> str:
-    """ "
+    """
     Функция переиспользует ранее написанные функции
     и возвращает исходную строку с замаскированным номером карты/счета.
     """
     right_data = data.split(" ")
     if right_data[0] == "Счет":
-        return f"Счет {mask_account_number(right_data[-1])}"
+        return f"Счет {mask_account_number (right_data[-1])}"
     else:
-        return f'{" ".join(right_data[:-1])} {mask_credit_card_number(right_data[-1])}'
+        return f'{" ".join (right_data[:-1])} {mask_credit_card_number (right_data[-1])}'
 
 
 def convert_datetime_to_date(datetime_string: str) -> str:
